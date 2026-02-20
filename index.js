cf7815f94011ed3b6026e379ae9ab0bffbcf652e
Dudu <dudu.cets@hotmail.com>
2026-02-20 20:32:34 -0300

    Add delete note endpoint

--- index.js
+++ index.js
@@ -15,12 +15,14 @@ app.listen(PORT, () => {
 });
 
 const notes = []
+let nextID = 0
 
 app.post("/note", (req, res) => {
     const note = {
+        id: nextID++,
         title: req.body.title,
         content: req.body.content,
-        creationDate: new Date()
+        creationDate: new Date(),
     }
     notes.push(note)
     res.status(201).json(note)
@@ -29,3 +31,20 @@ app.post("/note", (req, res) => {
 app.get("/notes", (req, res) => {
     res.status(200).json(notes)
 })
+
+app.delete("/note/:id", (req, res) => {
+    const idToRemove = Number(req.params.id)
+
+    let currentIndex = 0
+    for (const note of notes){
+        if (note.id === idToRemove){
+            notes.splice(currentIndex, 1)
+            res.status(200).send()
+            return
+        }
+        currentIndex++
+    }
+    res.status(404).send()
+})
+
+
